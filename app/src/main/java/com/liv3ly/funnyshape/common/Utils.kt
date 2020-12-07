61debf0939336bac916d35c24420686b8772dcdc
CuongHuynh <cuonghuynhvan@gmail.com>
2020-12-07 15:01:17 +0700

    fixing minor issue.

--- app/src/main/java/com/liv3ly/funnyshape/common/Utils.kt
+++ app/src/main/java/com/liv3ly/funnyshape/common/Utils.kt
@@ -3,6 +3,7 @@ package com.liv3ly.funnyshape.common
 import android.content.Context
 import android.util.Log
 import android.widget.FrameLayout
+import androidx.core.view.contains
 import com.liv3ly.funnyshape.widget.*
 import kotlin.math.roundToInt
 
@@ -14,15 +15,15 @@ class Utils {
             is Triangle -> TriangleView(context)
         }
 
-        fun addShapeViewIntoShapeLayout(shapeView: ShapeView, shapeLayout: ShapeLayout, shape: Shape): ShapeView {
+        fun addShapeViewIntoShapeLayout(shapeView: ShapeView, shapeLayout: ShapeLayout, shape: Shape) {
             val layoutParam: FrameLayout.LayoutParams =
                 FrameLayout.LayoutParams(shape.size, shape.size)
             layoutParam.leftMargin = (shape.centerX - shape.size / 2).roundToInt()
             layoutParam.topMargin = (shape.centerY - shape.size / 2).roundToInt()
 
-            shapeLayout.addView(shapeView, layoutParam)
-
-            return shapeView
+            if(!shapeLayout.contains(shapeView)) {
+                shapeLayout.addView(shapeView, layoutParam)
+            }
         }
 
         fun makeRandomInt(maxValue: Int): Int {
